ec86b350df46be3563edf6650ca74cfeb7e98771
Benoit Favre <benoit.favre@lis-lab.fr>
2019-05-25 11:45:46 +1000

    add windows dlls

--- build-mingw.sh
+++ build-mingw.sh
@@ -6,4 +6,4 @@ mkdir -p build-mingw
 cd build-mingw
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../cmake/mingw-toolchain.cmake -DSDL2_INCLUDE_DIR:PATH=/usr/x86_64-w64-mingw32/include/SDL2
 make -j 4
-cp /usr/x86_64-w64-mingw32/bin/SDL2.dll .
+cp ../platform/windows/*.dll .
